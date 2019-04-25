10c71e87481c418fda2399a88a2e654391169d95
Jonathan Ma <jonma1221@gmail.com>
2019-04-25 11:18:42 -0700

    inject repo dependency into view model

--- app/src/main/java/com/somenewsapp/ui/TopHeadlineViewModel.kt
+++ app/src/main/java/com/somenewsapp/ui/TopHeadlineViewModel.kt
@@ -1,4 +1,11 @@
 package com.somenewsapp.ui
 
-class TopHeadlineViewModel {
+import android.arch.lifecycle.ViewModel
+import com.somenewsapp.data.source.remote.HeadlineRepository
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class TopHeadlineViewModel @Inject constructor(repository: HeadlineRepository) : ViewModel() {
+
+    val compositeDisposable: CompositeDisposable = CompositeDisposable()
 }
