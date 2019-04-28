f6a5b91498c24cec4b79237c69ef49b4621b9493
Jonathan Ma <jonma1221@gmail.com>
2019-04-28 10:48:29 -0700

    added mutable live data for articles

--- app/src/main/java/com/somenewsapp/ui/TopHeadlineViewModel.kt
+++ app/src/main/java/com/somenewsapp/ui/TopHeadlineViewModel.kt
@@ -1,11 +1,15 @@
 package com.somenewsapp.ui
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.somenewsapp.data.model.ArticlesItem
+import com.somenewsapp.data.model.HeadlineResponse
 import com.somenewsapp.data.source.remote.HeadlineRepository
 import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
 
 class TopHeadlineViewModel @Inject constructor(repository: HeadlineRepository) : ViewModel() {
 
+    val headlineList = MutableLiveData<List<ArticlesItem>>()
     val compositeDisposable: CompositeDisposable = CompositeDisposable()
 }
